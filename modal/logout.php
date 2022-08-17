dedb0bf345fa22cc49b6d32fa968634d392eec4d
AlexKsenz <sany00ksenz@gmail.com>
2022-08-17 23:55:55 +0300

    Admin part

--- modal/logout.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  require('../partials/header.php');
-  if(isset($_SESSION["user_id"]) && $_SESSION["user_id"] != NULL){
-    $_SESSION["user_id"] = NULL;
-    header("Location: /");
-  }
-?>
